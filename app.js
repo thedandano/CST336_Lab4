1d5fed42b7ad78e94c90cd41c8c1f09580031814
Dan Sedano <dandano2006@gmail.com>
2020-07-12 21:52:25 -0700

    added content

--- app.js
+++ app.js
@@ -6,32 +6,31 @@ const app = express();
 app.engine("html", require("ejs").renderFile);
 //specify public folder (the folder that holds the static files like css and images)
 app.use(express.static("public"));
+//local dev variables
+require("dotenv").config();
 
 //global variables
 const PORT = process.env.PORT;
 const IP = process.env.IP;
-
-//routes
-app.get("/", function (req, res) {
-  // res.send("It works!"); // not practical lot of html code
-  res.render("index.html");
-});
 
 //starting server
 app.listen(PORT, IP, function () {
   console.log("Express server is running...");
 });
 
-// handles get requests for each page
+//routes
 app.get("/", function (req, res) {
-  res.send("It works!");
+  res.render("index.html", { page_name: "home" });
 });
 
 app.get("/mercury", function (req, res) {
-  // res.send("This will be Mercury web page!");
   res.render("mercury.html");
 });
 
 app.get("/venus", function (req, res) {
-  res.send("This will be Venus web page!");
+  res.render("venus.html");
+});
+
+app.get("/earth", function (req, res) {
+  res.render("earth.html");
 });
